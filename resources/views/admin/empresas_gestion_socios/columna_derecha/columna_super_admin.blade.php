3fa8393ad025e8a49cc8c483bbfdc5947bb27bc0
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-09-14 19:07:28 -0300

    ok

---
+++ resources/views/admin/empresas_gestion_socios/columna_derecha/columna_super_admin.blade.php
@@ -0,0 +1,24 @@
+ 
+@if(Auth::user()->role >= 7)
+ <ul >
+   <div class="admin-columna-ul-titulo">Super admin</div>
+   <div id="admin-col-superadmin">
+        
+        <a href="{{route('get_admin_users')}}">
+          <li class="admin-columna-li mi-float-right"><i class="fas fa-user"></i> Usuarios</li>
+        </a>
+        
+
+        
+        <a href="{{route('get_admin_empresas_gestion_socios')}}">
+          <li class="admin-columna-li mi-float-right"><i class="fas fa-bars"></i> Empresas gestión socios</li>
+        </a>
+
+        
+        
+    </div>
+
+</ul>
+@endif
+
+   
