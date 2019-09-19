c4820e81a45cdf7aa6f152b282dddff88b31da74
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-09-19 20:15:10 -0300

    fg

--- app/Entidades/UserEmpresa.php
+++ app/Entidades/UserEmpresa.php
@@ -28,7 +28,7 @@ class UserEmpresa extends Model
 
     public function user()
     {
-       /* return $this->belongsTo(User::class,'user_id','id');*/
+        return $this->belongsTo(User::class,'user_id','id');
     } 
 
       public function getUserAsociadoAttribute()
@@ -38,7 +38,7 @@ class UserEmpresa extends Model
 
     public function empresa()
     {
-       /* return $this->belongsTo(EmpresaConSocios::class,'empresa_id','id');*/
+        return $this->belongsTo(EmpresaConSocios::class,'empresa_id','id');
     }  
 
       public function getEmpresaAsociadaAttribute()
