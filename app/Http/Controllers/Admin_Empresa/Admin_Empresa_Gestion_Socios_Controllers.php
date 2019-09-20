858e6e6dd5f73f3568b3207f5486ecc7bcc8f76b
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-09-19 21:26:44 -0300

    df

--- app/Http/Controllers/Admin_Empresa/Admin_Empresa_Gestion_Socios_Controllers.php
+++ app/Http/Controllers/Admin_Empresa/Admin_Empresa_Gestion_Socios_Controllers.php
@@ -99,7 +99,7 @@ class Admin_Empresa_Gestion_Socios_Controllers extends Controller
   {
     $Empresa = $this->EmpresaConSociosoRepo->find($id);
 
-    dd($Empresa);
+    dd($Empresa->users);
 
     return view('admin.empresas_gestion_socios.empresa_gestion_socios_home_editar',compact('Empresa'));
   }
