254ceeb52d9f6105fd8bdddb0226a83e5dfe10c4
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-09-21 20:42:22 -0300

    ok

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/vincular-user-empresa.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/vincular-user-empresa.blade.php
@@ -165,7 +165,7 @@ template:'
       </div>
    </div>
    <div v-else>
-     No hay usuarios asociados     
+     No hay vendedores asociados     
    </div>
 
          <div class="modal fade" id="modal-vincular-usuario" tabindex="+1" role="dialog" aria-labelledby="myModalLabel">
