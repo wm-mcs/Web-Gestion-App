67e9413ab7954e4c8f946b3a579e0e60b6b186a3
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-10-07 20:03:54 -0300

    df

---
+++ resources/views/empresa_gestion_paginas/Vue_logica/instancia_users_vue.blade.php
@@ -0,0 +1,38 @@
+
+  
+
+   
+
+    var app = new Vue({
+    el: '#app',    
+    data:{     
+      
+
+
+      
+
+    },
+    mounted: function mounted () {        
+
+     
+
+
+    },
+
+    methods:{
+
+
+
+    cerrarModal:function(id_modal){
+
+     $(id_modal).modal('hide');
+     $('.modal-backdrop').remove();
+    },   
+    
+    }
+
+     
+
+   
+
+   });
