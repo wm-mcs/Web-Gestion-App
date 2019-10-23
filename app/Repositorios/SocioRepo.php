18e53652db3a3532e53479a740ca129644be767e
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-10-23 15:43:27 -0300

    fg

--- app/Repositorios/SocioRepo.php
+++ app/Repositorios/SocioRepo.php
@@ -21,27 +21,29 @@ class SocioRepo extends BaseRepo
 
   public function getSociosBusqueda($empresa_id, $palabra_de_busqueda,$cantidad_de_resultados = null)
   {
-       $socios_base  = $this->getEntidad()->active()->where('empresa_id',$empresa_id);
+       $socios_base         = $this->getEntidad()->active()->where('empresa_id',$empresa_id);
 
+       $palabra_de_busqueda = trim($palabra_de_busqueda);
+       
        if($palabra_de_busqueda != null)
        {        
        
 
        $array_de_ids = [];
 
-       $colecciones_separado  = $socios_base->where('name',$palabra_de_busqueda)->get();  
+       $colecciones_separado  = $socios_base->where('name', "LIKE",$palabra_de_busqueda)->get();  
        $array_de_ids          = $this->traer_poner_ids($colecciones_separado,$array_de_ids);
 
-       $colecciones_separado2 = $socios_base->where('rut',$palabra_de_busqueda)->get();  
+       $colecciones_separado2 = $socios_base->where('rut', "LIKE",$palabra_de_busqueda)->get();  
        $array_de_ids          = $this->traer_poner_ids($colecciones_separado2,$array_de_ids);
 
-       $colecciones_separado3 = $socios_base->where('razon_social',$palabra_de_busqueda)->get();  
+       $colecciones_separado3 = $socios_base->where('razon_social', "LIKE",$palabra_de_busqueda)->get();  
        $array_de_ids          = $this->traer_poner_ids($colecciones_separado3,$array_de_ids);
 
-       $colecciones_separado4 = $socios_base->where('email',$palabra_de_busqueda)->get();  
+       $colecciones_separado4 = $socios_base->where('email', "LIKE",$palabra_de_busqueda)->get();  
        $array_de_ids          = $this->traer_poner_ids($colecciones_separado3,$array_de_ids);
 
-       $colecciones_separado5 = $socios_base->where('celular',$palabra_de_busqueda)->get();  
+       $colecciones_separado5 = $socios_base->where('celular', "LIKE",$palabra_de_busqueda)->get();  
        $array_de_ids          = $this->traer_poner_ids($colecciones_separado3,$array_de_ids);
 
        $array_de_ids          = array_unique($array_de_ids); 
