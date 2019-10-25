22a7dd06c23355c5cbbb20b468cf01bcf403a74c
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-10-25 18:10:31 -0300

    df

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/ingresar_movimiento_caja.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/ingresar_movimiento_caja.blade.php
@@ -4,19 +4,37 @@ props:[ 'empresa' ],
 data:function(){
     return {
      modal:'#modal-ingreso-caja',
-     tipo_de_servicios: {!! json_encode(config('tipo_movimientos_de_caja')) !!}
+     tipos_de_servicios: {!! json_encode(config('tipo_movimientos_de_caja')) !!},
+     servicio_elegido:''
+
     }
 },
 methods:{
  abrir_modal:function(){
-
    $(this.modal).appendTo("body").modal('show'); 
-
-
  },
+ elegir_lo_que_voy_a_agregar:function(tipo_servicio){
+  this.servicio_elegido = tipo_servicio;
+ },
+ cancelarIngreso:function(){
+  this.servicio_elegido = '';
+ }
     
 
 },
+computed:{
+  servicio_elegido_comp:function(){
+    if(this.servicio_elegido != '')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
+,
 template:'<span >
    <div  class="admin-user-boton-Crear" v-on:click="abrir_modal" title="Ingresar un movimiento de caja">
        <i class="fas fa-cash-register"></i>
@@ -37,7 +55,15 @@ template:'<span >
           
         </div>
         <div class="modal-body text-center"> 
-
+         <div v-if="servicio_elegido_comp">
+           
+           <div v-on:click="cancelarIngreso">Cancelar</div>
+         </div>
+         <div v-else>
+           <div v-for="servicio in tipos_de_servicios" v-on:click="elegir_lo_que_voy_a_agregar(servicio)">
+             @{{servicio.nombre}}
+           </div>
+         </div>
                  
                  
         </div>
