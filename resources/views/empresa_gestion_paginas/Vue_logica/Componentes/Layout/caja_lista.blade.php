c44b851fad6863cdfa3b96ce2fa78e696b387fc3
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-10-25 14:02:52 -0300

    sd

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Layout/caja_lista.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Layout/caja_lista.blade.php
@@ -3,19 +3,29 @@ Vue.component('caja-lista' ,
 props:[ 'caja','sucursal' ],
 data:function(){
     return {
-     
+     mostrar:true;
 
     }
 },
-methods:{
-
+computed:{
+  getClassLista:function(){
+    return {
+      'sub-contiene-lista-caja': this.mostrar ,
+      'sub-contiene-lista-caja-deudor': this.caja.tipo_saldo === 'deudor',
+      'sub-contiene-lista-caja-acredor': this.caja.tipo_saldo === 'acredor',
+    }
+  }
     
 
 },
-template:' <div >              
-              <span>@{{caja.detalle}}</span>
-              <span>@{{caja.moneda}}</span>            
-              <span>@{{caja.valor}}</span>
+template:'  <div class="contiene-lista-caja"> 
+
+              <div :class="getClassLista">
+                <span>@{{caja.detalle}}</span>
+                <span>@{{caja.moneda}}</span>            
+                <span>@{{caja.valor}}</span>
+              </div>             
+              
             </div>'
 
 }
