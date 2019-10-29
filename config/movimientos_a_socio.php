838433cc42494e69468acabb15ccfa124fa5f70b
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-10-29 09:53:27 -0300

    df

---
+++ config/movimientos_a_socio.php
@@ -0,0 +1,19 @@
+<?php 
+
+return array(
+
+  [ 
+      'nombre'     => 'Venta Producto',
+      'tipo_saldo' => 'acredor',
+  ],
+  [ 
+      'nombre'     => 'Venta Indumentaria',
+      'tipo_saldo' => 'acredor',
+  ],
+  [ 
+      'nombre'     => 'Cobro',
+      'tipo_saldo' => 'deudor',
+  ]
+
+
+) ;
