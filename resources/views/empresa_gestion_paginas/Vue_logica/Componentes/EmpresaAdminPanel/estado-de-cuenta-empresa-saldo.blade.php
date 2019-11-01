e8f5c8e4cc08bb4436bb4524674799f95174ab53
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-11-01 09:38:43 -0300

    fg

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/EmpresaAdminPanel/estado-de-cuenta-empresa-saldo.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/EmpresaAdminPanel/estado-de-cuenta-empresa-saldo.blade.php
@@ -42,33 +42,33 @@ computed:{
 },
 template:'<span>
   
- <div v-if="!empresa.saldo_de_estado_de_cuenta_pesos == 0 || !empresa.saldo_de_estado_de_cuenta_dolares == 0" class="flex-row-center">
+ <div v-if="!empresa.estado_de_cuenta_saldo_pesos == 0 || !empresa.estado_de_cuenta_saldo_dolares == 0" class="flex-row-center">
             
          
-              <div v-if="es_mayor_que_sero(empresa.saldo_de_estado_de_cuenta_pesos)" class="estado-de-cuenta-saldo estado-pago-indication">
-                <span v-if="empresa.saldo_de_estado_de_cuenta_pesos == 0">
+              <div v-if="es_mayor_que_sero(empresa.estado_de_cuenta_saldo_pesos)" class="estado-de-cuenta-saldo estado-pago-indication">
+                <span v-if="empresa.estado_de_cuenta_saldo_pesos == 0">
                   Está al día <i class="far fa-grin"></i> (en pesos)
                 </span>
                 <span v-else>
-                  Tiene a favor $ @{{ Math.abs(socio.saldo_de_estado_de_cuenta_pesos)}} <i class="far fa-grin"></i>
+                  Tiene a favor $ @{{ Math.abs(socio.estado_de_cuenta_saldo_pesos)}} <i class="far fa-grin"></i>
                 </span>
                 
               </div>
               <div v-else class="estado-de-cuenta-saldo estado-debe-indication">
-                Debe pesos: $ @{{ Math.abs(empresa.saldo_de_estado_de_cuenta_pesos)}}  <i class="far fa-frown-open"></i>
+                Debe pesos: $ @{{ Math.abs(empresa.estado_de_cuenta_saldo_pesos)}}  <i class="far fa-frown-open"></i>
               </div>
 
-              <div v-if="es_mayor_que_sero(empresa.saldo_de_estado_de_cuenta_dolares)" class="estado-de-cuenta-saldo estado-pago-indication">
-                <span v-if="empresa.saldo_de_estado_de_cuenta_dolares == 0">
+              <div v-if="es_mayor_que_sero(empresa.estado_de_cuenta_saldo_dolares)" class="estado-de-cuenta-saldo estado-pago-indication">
+                <span v-if="empresa.estado_de_cuenta_saldo_dolares == 0">
                   Está al día <i class="far fa-grin"></i> (en dolares)
                 </span>
                 <span v-else>
-                  Tiene a favor U$S @{{Math.abs(empresa.saldo_de_estado_de_cuenta_dolares)}} <i class="far fa-grin"></i>
+                  Tiene a favor U$S @{{Math.abs(empresa.estado_de_cuenta_saldo_dolares)}} <i class="far fa-grin"></i>
                 </span>
                 
               </div>
               <div v-else class="estado-de-cuenta-saldo estado-debe-indication">
-                Debe pesos: U$S @{{Math.abs(empresa.saldo_de_estado_de_cuenta_dolares)}}  <i class="far fa-frown-open"></i>
+                Debe pesos: U$S @{{Math.abs(empresa.estado_de_cuenta_saldo_dolares)}}  <i class="far fa-frown-open"></i>
               </div>
 
            </div>   
