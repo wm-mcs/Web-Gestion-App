b0293abad13721501ede3bd4fc6df6ffc955243a
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-11-02 19:50:07 -0300

    df

--- resources/views/empresa_gestion_paginas/socio_panel.blade.php
+++ resources/views/empresa_gestion_paginas/socio_panel.blade.php
@@ -39,7 +39,7 @@
 
 
 <script type="text/javascript">
-
+      @include('empresa_gestion_paginas.Vue_logica.Componentes.sucursa-nav')
      @include('empresa_gestion_paginas.Vue_logica.Componentes.socio-panel.ingresar-movimiento-a-socio') 
      @include('empresa_gestion_paginas.Vue_logica.Componentes.Layout.caja_lista')
      @include('empresa_gestion_paginas.Vue_logica.Componentes.Layout.caja_saldo')
