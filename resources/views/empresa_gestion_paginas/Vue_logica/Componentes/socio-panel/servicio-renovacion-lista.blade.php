a96f50914f8dafb1eabba6eff8f069825b5a89c0
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-11-05 17:16:26 -0300

    fg

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-panel/servicio-renovacion-lista.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-panel/servicio-renovacion-lista.blade.php
@@ -3,13 +3,17 @@ Vue.component('servicio-renovacion-lista' ,
 props:[ 'empresa','servicio_renovacion' ],
 data:function(){
     return {
-     
+     se_renueva:this.servicio_renovacion.se_renueva_automaticamente,
 
     }
 },
 methods:{
 
-    
+    actualizar_renovacion:function(){
+
+      this.se_renueva = this.servicio_renovacion.se_renueva_automaticamente;
+      alert('hola');
+    }
 
 },
 template:'<div class="contiene-se-renueva">
@@ -21,15 +25,15 @@ template:'<div class="contiene-se-renueva">
    <div class="sub-contiene-se-renueva">
      
    
-              <span class="se-renueva-automaticamente">Se renueva automáticamente </span>
+              <span class="se-renueva-automaticamente">¿Se renueva automáticamente? </span>
         <div class="flex-row-center flex-justifice-space-around get_width_70">
            <div class="contiene-re-renueva-label-input">
-                <input type="radio" value="si" v-model="servicio_renovacion.se_renueva_automaticamente">
+                <input type="radio" value="si" v-on:change="actualizar_renovacion" v-model="servicio_renovacion.se_renueva_automaticamente">
                 <label class="renueva-label" for="si">Si</label>
               </div>
               
               <div class="contiene-re-renueva-label-input">
-                <input type="radio" value="no" v-model="servicio_renovacion.se_renueva_automaticamente">
+                <input type="radio" value="no" v-on:change="actualizar_renovacion" v-model="servicio_renovacion.se_renueva_automaticamente">
                 <label class="renueva-label" for="no">No</label>
               </div>
         </div>
