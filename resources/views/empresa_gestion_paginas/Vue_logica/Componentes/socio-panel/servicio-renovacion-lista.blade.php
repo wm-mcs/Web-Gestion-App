f09aec9e9499d9530b74b129294160f57b3c8756
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-11-05 16:09:14 -0300

    df

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-panel/servicio-renovacion-lista.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-panel/servicio-renovacion-lista.blade.php
@@ -12,15 +12,26 @@ methods:{
     
 
 },
-template:'<span>
+template:'<div class="">
 
   
-   <span>@{{servicio_renovacion.servicio_tipo.name}} </span>
-   <span>Ultima renovación:@{{servicio_renovacion.fecha_ultima_renovacion}} </span>
-   <span>Se renueva automáticamente:@{{servicio_renovacion.se_renueva_automaticamente}} </span>
+   <span class="se-renueva-name">@{{servicio_renovacion.servicio_tipo.name}} </span>
+   <span class="se-renueva-ULTIMA">Última renovación: @{{servicio_renovacion.fecha_ultima_renovacion}} </span>
+   <span >Se renueva automáticamente </span>
+   <div class="flex-row-center flex-justifice-space-around get_width_100">
+              <div class="contiene-opcion-moneda">
+                <input type="radio" value="si" v-model="servicio_renovacion.se_renueva_automaticamente">
+                <label class="moneda-label" for="si">Si</label>
+              </div>
+              
+              <div class="contiene-opcion-moneda">
+                <input type="radio" value="no" v-model="servicio_renovacion.se_renueva_automaticamente">
+                <label class="moneda-label" for="no">No</label>
+              </div>
+   </div>
   
 
-</span>'
+</div>'
 
 }
 
