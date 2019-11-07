00f9e3b6e8dc80f650bb276b507a8902aaf6a3f1
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-11-07 15:35:37 -0300

    df

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Layout/configuracion-empresa.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Layout/configuracion-empresa.blade.php
@@ -59,7 +59,10 @@ template:'  <span class="contiene-sucursal" v-on:click="abrir_modal"> <i class="
 
           <div class="contiene-linea-configuracion">
             <span class="titulo-linea-seccion">¿Los servicios se renuevan automáticamente? </span>
-            <span class="aclaracion-linea-seccion"></span>
+            <span class="aclaracion-linea-seccion">
+              Si ésta función está en “si”  el sistema de forma diaria ira buscando los socios que habían contratado previamente algún servicio mensual y se lo renovara de forma automática unos días antes de que se venza. La <strong>fecha de vencimiento del nuevo servicio será un mes posterior a la fecha de su último servicio</strong>  contratado. Esto generará un <strong>cargo para el socio y se verá reflejado en su estado de cuenta</strong> . En caso de que el socio tenga deudas, no se renovará de forma automática
+
+            </span>
             <div class="flex-row-center flex-justifice-space-around get_width_70">
               <div class="contiene-re-renueva-label-input">
                 <input type="radio" value="si" v-on:change="actualizar_datos" v-model="empresa.actualizar_servicios_socios_automaticamente">
