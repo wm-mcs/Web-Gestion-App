d51ab1f42d97574b49f8477418789f3df851a9cb
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-11-22 11:35:45 -0300

    fg

--- app/Entidades/ServicioContratadoEmpresa.php
+++ app/Entidades/ServicioContratadoEmpresa.php
@@ -3,6 +3,9 @@
 namespace App\Entidades;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+use App\Entidades\TipoDeServicioAEmpresas;
 
 
 
@@ -20,9 +23,25 @@ class ServicioContratadoEmpresa extends Model
      */
     protected $fillable = ['name'];
 
+    protected $appends  = [
+                           'fecha_vencimiento_formateada',
+                           'fecha_contratado_formateada',                           
+                           'tipo_de_servicio'
+                          ];
 
 
 
+    public function tipo_servicio()
+    {
+      return  $this->belongsTo(TipoDeServicioAEmpresas::class,'tipo_servicio_id','id'); 
+    }    
+
+      public function getTipoDeServicioAttribute()
+      {
+          return Cache::remember('tipoServicioEmpresa'.$this->id, 300, function() {
+                              return $this->tipo_servicio; 
+                          }); 
+      }                        
     
 
     
@@ -47,8 +66,17 @@ class ServicioContratadoEmpresa extends Model
     public function scopeActive($query)
     {
                                
-           $query->where('estado', "si"); 
-                
+           $query->where('estado', "si");  
+
+    }
+
+     public function getFechaVencimientoFormateadaAttribute()
+    {
+        return Carbon::parse($this->fecha_vencimiento)->format('Y-m-d');
+    }
+public function getFechaContratadoFormateadaAttribute()
+    {
+        return $this->created_at->format('Y-m-d'); 
     }
 
 
