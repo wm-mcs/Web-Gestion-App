3ec8f682b4c314ce7a961e865a0b3170a3ecb336
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-11-26 23:31:15 -0300

    df

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-entidad-listado-individual-componente.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-entidad-listado-individual-componente.blade.php
@@ -327,7 +327,7 @@ template:'
   </div>
   <div class="flex-row-center">
     <div class="simula_link margin-right-4px" v-on:click="cargar_servicios" title="Se agregaran los servicios de carácter mensual que usualmente contrata el socio. La fecha de vencimiento será a un mes posterior al vencimiento del último servicio que había contratado.  Si el usuario tiene deuda actualmente no se podrá generar hasta que quede al día. No se renovaran los servicios que son tipo clases, solo los mensuales."><i class="fas fa-cog"></i></div>
-    <estado-de-cuenta-socio-saldo :empresa="empresa" :socio="socio"> </estado-de-cuenta-socio-saldo>
+    <estado-de-cuenta-socio-saldo v-if="mostrarEstadoDeCuenta" :empresa="empresa" :socio="socio"> </estado-de-cuenta-socio-saldo>
   </div>
 
 
