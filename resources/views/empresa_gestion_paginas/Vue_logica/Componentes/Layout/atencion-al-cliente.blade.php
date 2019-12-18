27a6063e53f96969be63abf01eef39577dae64b7
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-12-18 19:06:00 -0300

    df

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Layout/atencion-al-cliente.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Layout/atencion-al-cliente.blade.php
@@ -15,10 +15,7 @@ methods:{
 template:'<div class="contacto-whatsapp-contenedor" >
   <img :src="empresa.vendedor_de_esta_empresa.foto_de_perfil" class="contacto-whatsapp-img">
 
-  <div class="contiene-aclaracio-y-resouesta">
-    <div class="aclaracion-de-respuesta">                      
-        Normalmente respondo en <span class="text-success">menos de 1 hora</span> 
-    </div>
+  <div class="contiene-aclaracio-y-resouesta">    
     <div class="contacto-whatsapp-texto">
     Hola ¿en qué te puedo ayudar? 
     </div> 
