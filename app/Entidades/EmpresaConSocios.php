f78c7adf73fe366c51df2f14f255d9c7a7cbc120
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-12-18 10:25:26 -0300

    df

--- app/Entidades/EmpresaConSocios.php
+++ app/Entidades/EmpresaConSocios.php
@@ -177,14 +177,14 @@ class EmpresaConSocios extends Model
          $VendedorRepo = new VendedorEmpresaRepo();
          $UserRepo     = new UserRepo(); 
 
-         return Cache::remember('VendedorDeEstaEmpresa'.$this->id, 8000, function() use($VendedorRepo , $UserRepo) {
+         /*return Cache::remember('VendedorDeEstaEmpresa'.$this->id, 8000, function() use($VendedorRepo , $UserRepo) {*/
 
          
          $VendedorId   = $VendedorRepo->getVendedoresDeEstaEmpresa($this->id); 
          $User         =  $UserRepo->find($VendedorId->first()->user_id);        
-        dd($User);
+       
          return $User ;
-         });  
+         /*}); */ 
     }
 
 
