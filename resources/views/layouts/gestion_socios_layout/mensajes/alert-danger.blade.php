5d60546fad946f9a873da3c262a57a37237555fb
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-01-31 15:05:34 -0300

    fg

--- resources/views/layouts/gestion_socios_layout/mensajes/alert-danger.blade.php
+++ resources/views/layouts/gestion_socios_layout/mensajes/alert-danger.blade.php
@@ -8,3 +8,15 @@
     </span> 
   </div>
 @endif
+
+
+@if(Session::has('alert'))
+            <div class="mensajes-contenedor ocultar-esto alert-success">
+
+               {{ Session::get('alert') }} 
+              
+             <span class="mensaje-cerrar-icono">
+                 <i class="fas fa-times"></i>
+              </span> 
+            </div>
+@endif 
