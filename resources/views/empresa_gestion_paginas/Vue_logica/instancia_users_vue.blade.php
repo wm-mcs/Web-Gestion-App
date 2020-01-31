6082013b1e3a18e6364127bb4bc386189c8db692
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-01-31 13:30:39 -0300

    fg

--- resources/views/empresa_gestion_paginas/Vue_logica/instancia_users_vue.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/instancia_users_vue.blade.php
@@ -7,14 +7,24 @@
     el: '#app',    
     data:{     
       
-    cargando:false,
+      variable:'esta es la instancia',
+      windowWidth: window.innerWidth,
+      resolucion_celular: 320,
+      resolucion_tablet: 640,
+      resolucion_pc: 1024,
+      menu_abierto:false,
+      cargando:false,
 
       
 
     },
     mounted: function mounted () {        
 
-     
+      this.$nextTick(() => {
+        window.addEventListener('resize', () => {
+          this.windowWidth = window.innerWidth
+        });
+      });
 
 
     },
@@ -30,8 +40,72 @@
     },   
     abrirModal:function(id_modal){
     $(id_modal).appendTo("body").modal('show');
+    },
+    abrir_menu_cerrar_principal:function(){
+      if(this.menu_abierto)
+      {
+        this.menu_abierto = false;
+      }
+      else
+      {
+        this.menu_abierto = true;
+      }
     }
     
+    
+    },
+    computed:{
+     whatsappContactoPagos:function(){
+     var url = 'https://api.whatsapp.com/send?phone=59892336388&text=Hola';
+     return url;
+    },
+    esResolucionDeCelular:function(){
+      if(this.windowWidth <= this.resolucion_celular)
+      {
+        return true;
+      }
+    },
+    esResolucionDeTablet:function(){
+      if(this.windowWidth <= this.resolucion_tablet)
+      {
+        return true;
+      }
+    },
+    esResolucionDePc:function(){
+      if(this.windowWidth > this.resolucion_pc)
+      {
+        return true;
+      }
+    },
+    mostrar_menu:function(){
+      if(this.menu_abierto)
+      {
+        return true;
+      }
+      if(this.esResolucionDePc)
+      {
+        return true;
+      }
+
+      if(!this.menu_abierto)
+      {
+        return false;
+      }
+    },
+    contenido_style_width:function(){
+      if(this.esResolucionDePc)
+      {
+        return {
+                  width: '80%',
+               }
+      }
+      else
+      {
+        return {
+                  width: '100%',
+               }
+      }
+    }
     }
 
      
@@ -39,3 +113,13 @@
    
 
    });
+
+
+
+
+
+
+
+
+
+
