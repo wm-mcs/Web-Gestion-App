c9795a98417ed4ed54e53c815a21cbca098991e2
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-02-02 21:19:49 -0300

    ok

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-entidad-listado-componente.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-entidad-listado-componente.blade.php
@@ -6,10 +6,15 @@ props:['empresa','palabra_busqueda']
 
 data:function(){
     return {
-         socios:{!! json_encode($Socios) !!}
+         socios:'',
+         cargando:false;
 
     }
 }, 
+
+mounted:{
+  
+},
 
 watch:{ 
 
@@ -28,6 +33,40 @@ methods:{
          
 actualizar_socios:function(socios){
 	this.socios = socios;
+},
+
+get_socios:function(){
+
+  var url = '/get_socios_activos';
+
+      var data = {  
+                    empresa_id: this.empresa.id       
+                 };  
+      var vue = this;  
+      this.cargando = true;         
+
+     axios.post(url,data).then(function (response){  
+            var data = response.data;  
+            
+
+            if(data.Validacion == true)
+            {
+               this.cargando = false; 
+               vue.socios = response.data.Socios;              
+               
+            }
+            else
+            {
+              this.cargando = false; 
+              $.notify(response.data.Validacion_mensaje, "error");
+            }
+           
+           }).catch(function (error){
+
+                     
+            
+           });
+  
 },
 checkSearchStr: _.debounce(function(string){
 
@@ -76,9 +115,11 @@ template:'
 <div v-if="socios.length" class="empresa-contendor-de-secciones">
   <div class="titulo-socios-cuando-hay"><i class="fas fa-users"></i> Socios  <i class="far fa-hand-point-down"></i></div>
 
-  <div v-if="socios.length > 0" class="listado-socios-contenedor-lista">
+  <div v-if="socios.length > 0 || !cargando" class="listado-socios-contenedor-lista">
 
-    <socio-list v-for="socio in socios" 
+    <socio-list 
+
+    v-for="socio in socios" 
                  :key="socio.id" 
                :socio="socio" 
              :empresa="empresa"
@@ -86,6 +127,14 @@ template:'
 
   
   </div> 
+  <div v-else class="Procesando-text">
+     
+                       <div class="cssload-container">
+                             <div class="cssload-tube-tunnel"></div>
+                       </div>
+                  
+    
+  </div>
 </div>  
 <div v-else class="cuando-no-hay-socios">
   No hay socios <i class="far fa-frown"></i>
