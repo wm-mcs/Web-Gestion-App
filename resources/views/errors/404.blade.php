27e502d4a3bf43b4d294a65fb53f61422250c05d
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-02-05 13:56:27 -0300

    df

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -27,7 +27,7 @@
           <br>
           <div class="error-404-titulo">Ups! </div>
           <img class="error-404-img" src="{{url()}}/imagenes/Errores/desechufe.png">
-          <div class="error-404-aclaracion">Nos confundimos :-( simplemente regresá dando <strong><a href="{{route('get_home')}}">click aquí</a></strong>  </div>
+          <div class="error-404-aclaracion">Nos confundimos. Regresá dando <strong><a href="{{route('get_home')}}">click aquí</a></strong>  </div>
           
           
           <a href="{{route('get_home')}}" class="error-404-contiene-boton">            
