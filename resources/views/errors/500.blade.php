28aa8e7c7bd6683eb330ebf62ad5a1c839007ab5
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-02-05 15:36:55 -0300

    df

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,43 +1,52 @@
-<html>
-    <head>
-        <title>Be right back.</title>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Upssss</title>
+    <link rel="stylesheet" type="text/css" href="{{url()}}{{ elixir('css/admin.css') }}">  
+    <link rel="shortcut icon" href="{{ asset('imagenes/favicon.ico') }}"> 
 
-        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"> 
+    
+    <META name="robots" content="NOINDEX,NOFOLLOW">
 
-        <style>
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                height: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
 
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                 <div class="title">Algo salió mal :( , pero ya estamos trabajando en eso. <a href="{{route('get_home')}}"> Volver a inicio </a></div>
-            </div>
+  </head>
+
+  <body>  
+
+
+     <div class="contiene-errores-general">
+
+        <div class="sub-contiene-errores-general">
+          <a href="{{route('get_home')}}"><img class="error-404-img-logo" src="{{url()}}/imagenes/Empresa/logo_rectangular.png"></a>
+          <br>
+          <div class="error-404-titulo">Ups! </div>
+          <img class="error-404-img" src="{{url()}}/imagenes/Errores/desechufe.png">
+          <div class="error-404-aclaracion">Nos confundimos. Regresá dando <strong><a href="{{route('get_home')}}">click aquí</a></strong>  </div>
+          
+          
+          <a href="{{route('get_home')}}" class="error-404-contiene-boton">            
+            Volver al panel administrador
+          </a>
+
+          
+
         </div>
-    </body>
+       
+     </div>
+  
+  
+ 
+   
+
+
+
+      
+  </body>
+
 </html>
