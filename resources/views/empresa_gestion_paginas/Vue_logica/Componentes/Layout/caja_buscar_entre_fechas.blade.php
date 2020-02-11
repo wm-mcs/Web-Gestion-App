6efddfdbfbfd3a8e493afbdd742504db29f124d4
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-02-10 23:40:28 -0300

    ty

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Layout/caja_buscar_entre_fechas.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Layout/caja_buscar_entre_fechas.blade.php
@@ -3,6 +3,6 @@
 	<input type="date" class="form-control get_width_40" v-model="fecha_fin" name="">
 	<div class="admin-user-boton-Crear" v-on:click="getMovimientosDeCaja"><i class="fas fa-search"></i></div>
 </div>
-<div v-else class="get_width_100 text-center" v-on:click="mostrar_busqueda" class="contiene-buscar-entre-fechas-texto">
+<div v-else  v-on:click="mostrar_busqueda" class="contiene-buscar-entre-fechas-texto">
 	Filtrar por fechas
 </div>
