75d25abcf5bc7e172213aee74c8c47f653a75cd4
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-02-11 20:53:51 -0300

    df

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Layout/caja_buscar_entre_fechas.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Layout/caja_buscar_entre_fechas.blade.php
@@ -4,21 +4,26 @@
 		<div class="fechas-buscar-texto">
 			Buscar los movimientos entre fechas.
 		</div>
-		<div class="flex-row-column get_width_100">
+		<div class="get_width_100 flex-row-center">
+			<div class="flex-row-column">
 			<input type="date" class="form-control fecha_input_caja_busqueda" v-model="fecha_inicio" name="">
 			<input type="date" class="form-control fecha_input_caja_busqueda" v-model="fecha_fin" name="">
 		</div>
 		<div class="admin-user-boton-Crear" v-on:click="disparador(entre_fechas)"><i class="fas fa-search"></i> </div>
+		</div>
+		
 	</div>
 	<div class="contiene-buscar-entre-fechas">
 		<div class="fechas-buscar-texto">
 			Ver los movimientos de ese día y el saldo a ese día.
 		</div>
+		<div class="get_width_100 flex-row-center">
 		<div class="flex-row-column get_width_100">
 			<input type="date" class="form-control fecha_input_caja_busqueda" v-model="fecha_inicio" name="">
 			
 		</div>
-		<div class="admin-user-boton-Crear" v-on:click="disparador(arqueo)">  </div>
+		<div class="admin-user-boton-Crear" v-on:click="disparador(arqueo)"> <i class="fas fa-search"></i> </div>
+		</div>
 	</div>
 </div>
 <div v-else  v-on:click="mostrar_busqueda" class="contiene-buscar-entre-fechas-texto">
