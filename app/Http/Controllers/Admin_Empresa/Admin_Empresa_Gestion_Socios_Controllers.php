4ab45e233ffaab49fe3f404bfd87dc28c90b3cd7
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-02-11 21:03:12 -0300

    df

--- app/Http/Controllers/Admin_Empresa/Admin_Empresa_Gestion_Socios_Controllers.php
+++ app/Http/Controllers/Admin_Empresa/Admin_Empresa_Gestion_Socios_Controllers.php
@@ -1108,7 +1108,7 @@ class Admin_Empresa_Gestion_Socios_Controllers extends Controller
      else
      {
        $Fecha_fin         = Carbon::parse($Request->get('fecha_fin'))->endOfDay();
-       $Fecha_inicio      = $Fecha_fin->subDays(14)->startOfDay());
+       $Fecha_inicio      = $Fecha_fin->subDays(14)->startOfDay();
        
      
      }
