5ffcdb7b5eb10cc9b2bdfcb2ad752b538fdf7ff9
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-02-24 13:28:19 -0300

    sd

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-entidad-listado-componente.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-entidad-listado-componente.blade.php
@@ -160,7 +160,7 @@ compara_valor_de_vencimiento:function(a,b){
     return 0;
   }
 
-  var order = 'asc';
+  
 
     const varA =  new Date(a.servicios_contratados_del_socio.[0].fecha_vencimiento);
     const varB = new Date(b.servicios_contratados_del_socio.[0].fecha_vencimiento);
@@ -174,9 +174,8 @@ compara_valor_de_vencimiento:function(a,b){
       comparison = -1;
     }
     console.log(varA,varB,comparison);
-    return (
-      (order === 'desc') ? (comparison * -1) : comparison
-    );
+
+    return comparison;
   
   
 },
