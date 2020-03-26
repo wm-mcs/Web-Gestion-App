a42cccb3fbab48b564519d4bb4ae5e3f45c7893e
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-03-26 18:10:30 -0300

    df

--- app/Http/Middleware/ApiPublica.php
+++ app/Http/Middleware/ApiPublica.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
 
 use Closure;
 
@@ -9,7 +11,29 @@ class ApiPublica
                          
     public function handle($request, Closure $next)
     {
-       
+        $ip = $request->header('ip_del_que_navega');
+        if($ip != null)
+        {
+          $nombre = 'ip_del_consultante_'.$ip;
+          if(Cache::has($nombre))
+          {
+            
+            Cache::increment($nombre);
+
+          }
+          else
+          {
+            Cache::put($nombre,1,Carbon::now('America/Montevideo')->addMinutes(5));
+          }
+
+          if(Cache::get($nombre) > 5)
+          {
+             return ['Validacion'          => false,
+                     'Validacion_mensaje'  => 'Muchas solicitudes.'];
+          }
+        }
+
+
 
         return $next($request);
     }
