15e1a6dadad92f5da006d8290019b9929a635990
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-14 17:55:12 -0300

    df

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
 
 @section('content') 
 <div class="container">
-	<div class="row justify-content-center py-2 py-lg-5">
+	<div class="d-flex flex-column align-items-center py-2 py-lg-5">
 		<img class="col-9 col-lg-4 img-fluid p-2 p-lg-5 mb-5" src="{{url()}}/imagenes/Empresa/logo_rectangular.png">
 		<div class="col-12 col-lg-5">  
          <h1 class="sub-titulos-class font-secondary text-color-black text-center mb-5">Inicio de sesión</h1>      
