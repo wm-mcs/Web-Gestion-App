e8dc4a9d7a1cc9fae0f8cb3bd01889b9161b10bc
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-14 17:50:32 -0300

    fg

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,9 +19,9 @@
 @section('content') 
 <div class="container">
 	<div class="row justify-content-center py-2 py-lg-5">
-		<img class="col-11 col-lg-6 img-fluid p-2 p-lg-5 mb-3" src="{{url()}}/imagenes/Empresa/logo_rectangular.png">
-		<div class="col-12 col-lg-7">  
-         <h1 class="sub-titulos-class font-secondary text-color-black text-center mb-4">Inicio de sesión</h1>      
+		<img class="col-9 col-lg-4 img-fluid p-2 p-lg-5 mb-5" src="{{url()}}/imagenes/Empresa/logo_rectangular.png">
+		<div class="col-12 col-lg-5">  
+         <h1 class="sub-titulos-class font-secondary text-color-black text-center mb-5">Inicio de sesión</h1>      
          @include('formularios.auth.login_form')
       </div>
 	</div>
