900e3a9876244bdbd24e395328b05fac3561e56c
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-14 17:47:43 -0300

    df

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,9 +17,16 @@
 
 
 @section('content') 
-      <img class="img-fluid p-2 p-lg-5 mb-2" src="{{url()}}/imagenes/Empresa/logo_rectangular.png">
-      <div class="contiene_auth_form">  
-         <h1 class="auth_titulo_h1">Inicio de sesión</h1>      
+<div class="container">
+	<div class="row justify-content-center py-2 py-lg-5">
+		<img class="col-11 col-lg-6 img-fluid p-2 p-lg-5 mb-3" src="{{url()}}/imagenes/Empresa/logo_rectangular.png">
+		<div class="col-12 col-lg-7">  
+         <h1 class="sub-titulos-class font-secondary text-color-black text-center mb-4">Inicio de sesión</h1>      
          @include('formularios.auth.login_form')
       </div>
+	</div>
+	
+</div>
+      
+      
 @stop
--- resources/views/formularios/auth/login_form.blade.php
+++ resources/views/formularios/auth/login_form.blade.php
@@ -1,50 +1,37 @@
-
-
-  {!! Form::open(             ['route' => 'auth_login_post',
+{!! Form::open(             ['route' => 'auth_login_post',
                             'method'   => 'post',
                             'files'    => true
                             ])               !!}
 
             
 <div class="text-center">
-<div class="center-block">
-            <div class="form-group">
-              
-              <div class="cols-sm-10">
-                <div class="input-group">
-                  <span class="input-group-addon"><i class="glyphicon glyphicon-user" aria-hidden="true"></i></span>
-                  
-                  {!! Form::text('email', null ,['class'       => 'form-control',
-                                                 'id'          => 'username',
-                                                 'placeholder' => 'Escribe tu email']) !!}
-                </div>
-              </div>
-            </div>
-
-            <div class="form-group">
-              
-              <div class="cols-sm-10">
-                <div class="input-group">
-                  <span class="input-group-addon"><i class="glyphicon glyphicon-lock" aria-hidden="true"></i></span>                
-                  {!! Form::password('password', [ 'class'       => 'form-control',
-                                                   'id'          => 'password',
-                                                   'placeholder' => 'Escribe tu contraseña']) !!}
-                </div>
-              </div>
-            </div>
-
-            <div class="form-group ">
-              <button type="submit" class="btn btn-primary btn-lg btn-block">Ingresar</button>
-            </div>
-            
-
-
-    <hr>
-   <div class="login-register">
-                    <p class="text-center"><a href="{{route('password_recet_get')}}">Olvidé mi contraseña</a><!--  | <a href="{{route('register_get')}}">Aún no estoy registrado</a> --></p>
-   </div>
-   
-</div>
+  <div class="center-block">
+      <div class="form-group">
+        <div class="cols-sm-10">
+          <div class="input-group"> 
+            {!! Form::text('email', null ,['class'       => 'input-text-class-primary',
+                                           'id'          => 'username',
+                                           'placeholder' => 'Escribe tu email']) !!}
+          </div>
+        </div>
+      </div>
+      <div class="form-group">              
+        <div class="cols-sm-10">
+          <div class="input-group">                                
+            {!! Form::password('password', [ 'class'       => 'input-text-class-primary',
+                                             'id'          => 'password',
+                                             'placeholder' => 'Escribe tu contraseña']) !!}
+          </div>
+        </div>
+      </div>
+      <div class="form-group ">
+        <button type="submit" class="Boton-Primario-Relleno Boton-Fuente-Chica">Ingresar</button>
+      </div>
+     <hr>
+     <div class="login-register">
+       <p class="text-center"><a href="{{route('password_recet_get')}}">Olvidé mi contraseña</a></p>
+     </div>   
+  </div>
 </div>
 
 
