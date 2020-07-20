9a399ae7cfdc07a1592cbe6467369056681a12bd
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-19 22:37:46 -0300

    df

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,16 +17,16 @@
 
 
 @section('content') 
-<div class="container d-flex flex-row alig-items-center justify-content-center">
-	<div class="d-flex flex-column align-items-center p-2 py-lg-4 background-gris-0">
+<div class="row d-flex flex-row alig-items-center justify-content-center">
+	<div class="d-flex flex-column align-items-center p-2 py-lg-4 ">
 		<div class="col-4 col-lg-4 p-2 p-lg-5 my-5">
 			<img class=" img-fluid " src="{{url()}}/imagenes/Empresa/logo_rectangular.png">
 		</div>
 		
-		<div class="col-11 col-lg-5">  
+		<div class="col-11 col-lg-5 background-gris-0 p-2 p-lg-4">  
          <h1 class="sub-titulos-class font-secondary text-color-black text-center mb-5">Inicio de sesión</h1>      
          @include('formularios.auth.login_form')
-      </div>
+       </div>
 	</div>
 	
 </div>
--- resources/views/layouts/user_layout/user_layout.blade.php
+++ resources/views/layouts/user_layout/user_layout.blade.php
@@ -1,66 +1,43 @@
 <!DOCTYPE html>
 <html lang="es">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-      <!-- META-TAGS DE FAVICONS, DISPOSITIVOS Y THUMBNAILS -->
-      <!-- social media meta-tags for proper sharing
-      (la imagen tiene que estar en la carpeta root, ratio 1.91:1 1200x630px) -->
+   @yield('og-tags')
 
-      @yield('og-tags')
+   <link rel="shortcut icon" href="{{ asset('imagenes/favicon.ico') }}"> 
 
-      <link rel="shortcut icon" href="{{ asset('imagenes/favicon.ico') }}"> 
-{{--*/ $ImagenParaTaG         = url() . '/imagenes/EasySocio/easy-socio-el-software-para-administrar-gimnasios-academias-de-baile-institutos-de-ingles-y-mucho-más.jpg'/*--}}
-{{--*/ $Titulo                = 'Bienvenido a una nueva forma de administrar tu emprendimiento' /*--}}
-{{--*/ $DescriptionEtiqueta   = 'Si tienes un gimnsasio, una academia de baile, centro/escuela de danzas y quieres un sistema que te ayude a administrar tu negocio, estás en el lugar correcto porque te voy a contar de qué trata EasySocio' /*--}}
-{{--*/ $PalabrasClaves        = '' /*--}}
-{{--*/ $UrlDeLaPagina         = url() /*--}}
- <meta property="og:type"               content="website" />
- <meta property="og:title"              content="{{ $Titulo}} " />
- <meta property="og:description"        content="{{$DescriptionEtiqueta}}" />
- <meta property="og:image"             content="{{$ImagenParaTaG }}" />
- <meta property="og:image:secure_url"  content="{{$ImagenParaTaG }}" /> 
- <meta property="og:image:width" content="250">
-      
-      
-
-      <title> @yield('title') | EasySocio</title>
-      <meta name="Description" CONTENT="@yield('MetaContent')">      
-      <META name="robots" content="@yield('MetaRobot')">
-      <meta name="Keywords"  content="@yield('palabras-claves')">
-
-      {{-- css --}}   
-      @include('layouts.user_layout.css_fonts')
-
-  
-  </head>
-  
-  <body > 
-
-     
-
-
-           <span id="app">
-             
-             <div class="flex-row-column get_width_100{{-- wraper-content-principal-con-nav --}}">
-              <div class="mensaje-contenedor-layout-auth">
-                @include('layouts.gestion_socios_layout.mensajes.mensajes')   
-              </div>
-                <div class="flex-row-column get_width_100">
-                  @yield('content')  
-                </div>
-
-                  
-             </div>           
-             
-        </span>
-
-
-    <!-- Scripts -->
-    <script src="{{url()}}{{ elixir('js/admin.js')}} " ></script>  
+   <meta property="og:type"               content="website" />
+   <meta property="og:title"              content="{{ $Titulo}} " />
+   <meta property="og:description"        content="{{$DescriptionEtiqueta}}" />
+   <meta property="og:image"             content="{{$ImagenParaTaG }}" />
+   <meta property="og:image:secure_url"  content="{{$ImagenParaTaG }}" /> 
+   <meta property="og:image:width" content="250">    
     
 
+   <title> @yield('title') | EasySocio</title>
+   <meta name="Description" CONTENT="@yield('MetaContent')">      
+   <META name="robots" content="@yield('MetaRobot')">
+   <meta name="Keywords"  content="@yield('palabras-claves')">
+
+   {{-- css --}}   
+   @include('layouts.user_layout.css_fonts')
+  </head>
+    
+  <body> 
+    <span id="app">
+       <div class="w-100 d-flex flex-column align-items-center">
+        <div class="mensaje-contenedor-layout-auth">
+          @include('layouts.gestion_socios_layout.mensajes.mensajes')   
+        </div>
+        <div class="w-100 d-flex flex-column align-items-center">
+          @yield('content')  
+        </div>
+       </div>  
+   </span>
+   <!-- Scripts -->
+   <script src="{{url()}}{{ elixir('js/admin.js')}} " ></script> 
   </body>
 </html>   
