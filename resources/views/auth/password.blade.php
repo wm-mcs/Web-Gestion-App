86d808f54c4c6667055cc4d4e4da257af08f6adf
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-27 19:35:15 -0300

    fg

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -18,17 +18,21 @@
 
 @section('content')
 
-
-
-
-
-<a href="{{route('get_home')}}">
- <img class="auth-logo-easy" src="{{url()}}/imagenes/Empresa/logo_rectangular.png">
-</a>
-<div class="contiene_auth_form">
- <h1 class="auth_titulo_h1">Recuperar contraseña  </h1>
-  @include('formularios.auth.reset_password_form')
+<div class="d-flex flex-row align-items-center justify-content-center">
+	<div class="d-flex flex-column align-items-center p-2 py-lg-4 ">
+		<div class="col-6 col-lg-4 p-2 p-lg-5 my-5 my-lg-3">
+			<img class=" img-fluid " src="{{url()}}/imagenes/Empresa/logo_rectangular.png">
+		</div>		
+		<div class="col-11 col-lg-5 background-gris-0 p-5 ">  
+         <h1 class="sub-titulos-class font-secondary text-color-black text-center mt-1 mb-3">Recuperar contraseña</h1>      
+          @include('formularios.auth.reset_password_form')
+       </div>
+	</div>
 </div>
+
+
+
+
 
      
 
