07c1d6a7e617ad5ff27ada56da7376b723ce1c49
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-27 19:21:43 -0300

    gh

--- resources/views/layouts/user_layout/user_layout.blade.php
+++ resources/views/layouts/user_layout/user_layout.blade.php
@@ -4,42 +4,36 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    @yield('og-tags')
+    <link rel="shortcut icon" href="{{ asset('imagenes/favicon.ico') }}"> 
+    {{--*/ $ImagenParaTaG         = url() . '/imagenes/EasySocio/easy-socio-el-software-para-administrar-gimnasios-academias-de-baile-institutos-de-ingles-y-mucho-más.jpg'/*--}}
+    {{--*/ $Titulo                = 'Bienvenido a una nueva forma de administrar tu emprendimiento' /*--}}
+    {{--*/ $DescriptionEtiqueta   = 'Si tienes un gimnsasio, una academia de baile, centro/escuela de danzas y quieres un sistema que te ayude a administrar tu negocio, estás en el lugar correcto porque te voy a contar de qué trata EasySocio' /*--}}
+    {{--*/ $PalabrasClaves        = '' /*--}}
+    {{--*/ $UrlDeLaPagina         = url() /*--}}
 
+     <meta property="og:type"               content="website" />
+     <meta property="og:title"              content="{{ $Titulo}} " />
+     <meta property="og:description"        content="{{$DescriptionEtiqueta}}" />
+     <meta property="og:image"             content="{{$ImagenParaTaG }}" />
+     <meta property="og:image:secure_url"  content="{{$ImagenParaTaG }}" /> 
+     <meta property="og:image:width" content="250">      
 
-      @yield('og-tags')
+    <title> @yield('title') | EasySocio</title>
+    <meta name="Description" CONTENT="@yield('MetaContent')">      
+    <META name="robots" content="@yield('MetaRobot')">
+    <meta name="Keywords"  content="@yield('palabras-claves')">
 
-      <link rel="shortcut icon" href="{{ asset('imagenes/favicon.ico') }}"> 
-      {{--*/ $ImagenParaTaG         = url() . '/imagenes/EasySocio/easy-socio-el-software-para-administrar-gimnasios-academias-de-baile-institutos-de-ingles-y-mucho-más.jpg'/*--}}
-      {{--*/ $Titulo                = 'Bienvenido a una nueva forma de administrar tu emprendimiento' /*--}}
-      {{--*/ $DescriptionEtiqueta   = 'Si tienes un gimnsasio, una academia de baile, centro/escuela de danzas y quieres un sistema que te ayude a administrar tu negocio, estás en el lugar correcto porque te voy a contar de qué trata EasySocio' /*--}}
-      {{--*/ $PalabrasClaves        = '' /*--}}
-      {{--*/ $UrlDeLaPagina         = url() /*--}}
-
-       <meta property="og:type"               content="website" />
-       <meta property="og:title"              content="{{ $Titulo}} " />
-       <meta property="og:description"        content="{{$DescriptionEtiqueta}}" />
-       <meta property="og:image"             content="{{$ImagenParaTaG }}" />
-       <meta property="og:image:secure_url"  content="{{$ImagenParaTaG }}" /> 
-       <meta property="og:image:width" content="250">      
-
-      <title> @yield('title') | EasySocio</title>
-      <meta name="Description" CONTENT="@yield('MetaContent')">      
-      <META name="robots" content="@yield('MetaRobot')">
-      <meta name="Keywords"  content="@yield('palabras-claves')">
-
-      {{-- css --}}   
-      @include('layouts.user_layout.css_fonts')
-
-  
+    {{-- css --}}   
+    @include('layouts.user_layout.css_fonts')  
   </head>
-  
   <body> 
     <span id="app">
-       <div class="w-100 d-flex flex-column align-items-center">
-        <div class="mensaje-contenedor-layout-auth">
-          @include('layouts.gestion_socios_layout.mensajes.mensajes')   
-        </div>
+       <div style="height: 100vh;" class="w-100 d-flex flex-column align-items-center">        
         <div class="w-100 d-flex flex-column align-items-center">
+          <div class="mensaje-contenedor-layout-auth">
+            @include('layouts.gestion_socios_layout.mensajes.mensajes')   
+          </div>
           @yield('content')  
         </div>
        </div>  
