1b33ef9791c6afa75ce4624e8785037a7d948036
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-27 19:53:07 -0300

    df

---
+++ resources/views/errors/partial_pagina_de_error.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Upssss</title>
+    @include('layouts.user_layout.css_fonts')  
+    <link rel="shortcut icon" href="{{ asset('imagenes/favicon.ico') }}"> 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">     
+    <META name="robots" content="NOINDEX,NOFOLLOW">
+  </head>
+  <body>  
+    <div class="w-100 d-flex flex-row align-items-center justify-content-center" style="height: 100vh;">
+     
+         <div class="p-5 d-flex flex-column align-items-center w-100 w-lg-75">
+          <div class="w-100 w-lg-75 mb-5">
+            <a class="" href="{{route('get_home')}}">
+             <img class="img-fluid" src="{{url()}}/imagenes/Empresa/logo_rectangular.png">
+            </a>  
+          </div>
+                  
+          <div class="color-text-gris titulos-class text-center mb-0">Ups!!! </div>
+          <div class="w-100 w-lg-75 mb-0" >
+            <img class="img-fluid" src="{{url()}}/imagenes/Errores/desechufe.png">
+          </div>          
+          <div class="color-text-gris parrafo-class text-center mb-5">Nos confundimos. Regresá dando <strong><a href="{{route('get_home')}}">click aquí</a></strong> 
+           </div>  
+          <a  href="{{route('get_home')}}" class="Boton-Fuente-Chica Boton-Primario-Relleno">            
+            Volver al panel administrador <i class="fas fa-angle-double-right"></i>
+          </a>    
+        </div>
+           
+    </div>      
+  </body>
+</html>
