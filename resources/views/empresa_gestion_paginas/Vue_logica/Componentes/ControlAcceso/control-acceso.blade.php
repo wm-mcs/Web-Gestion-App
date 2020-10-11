77c1bec1a681b4f944c56379b6e20758617f24a9
Mauricio <mauricio@worldmaster.com.uy>
2020-10-11 19:39:13 -0300

    fg

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/ControlAcceso/control-acceso.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/ControlAcceso/control-acceso.blade.php
@@ -25,13 +25,17 @@ template:`
 </div>
 <div v-else class="w-100 d-flex flex-column align-items-center">
           
+
+   <div class="col-8 col-lg-5 d-flex flex-column align-items-center">
     @if(file_exists($Empresa->path_url_img)) 
     <img class="my-3 controll-access-empresa-cliente-logo" src="{{$Empresa->url_img}}">
     @endif
     
     <h1 class="sub-titulos-class text-center">Control de acceso</h1>
 
-    <input class="controll-access-input-celular" v-model="celular" type="number"  placeholder="Escribe tu celular">
+    <input class="controll-access-input-celular my-4" v-model="celular" type="number"  placeholder="Escribe tu celular">
+   </div> 
+   
 
           
 
