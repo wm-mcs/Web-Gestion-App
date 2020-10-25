b2ca953199063a317c4991b3208a4a0a39eabed9
Mauricio <mauricio@worldmaster.com.uy>
2020-10-25 17:48:25 -0300

    fg

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/ingresar_movimiento_caja.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/ingresar_movimiento_caja.blade.php
@@ -4,6 +4,7 @@ props:[ 'empresa','sucursal'],
 data:function(){
     return {
      cargando:false, 
+     showModal:false,
      modal:'#modal-ingreso-caja',
      tipos_de_movimientos: [],
      servicio_elegido:'',
@@ -141,64 +142,77 @@ computed:{
 }
 ,
 template:`<span >
-   <div  class="admin-user-boton-Crear mr-lg-2" v-on:click="abrir_modal" title="Ingresar un movimiento de caja">
-       <i class="fas fa-cash-register"></i>
-   </div>
+<div   class="admin-user-boton-Crear"  v-on:click="showModal = !showModal" title="Ingreso de movimiento de caja">
+    <i class="fas fa-cash-register"></i>   
+</div>
+<transition name="modal" v-if="showModal">
+<div class="modal-mask ">
+  <div class="modal-wrapper">
+    <div class="modal-container position-relative">
+    <span class="modal-cerrar-icono sub-titulos-class text-center color-text-gris" @click="showModal = !showModal">
+      <i class="fas fa-times"></i>
+    </span>
 
-    <div class="modal fade" id="modal-ingreso-caja" tabindex="+1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title" id="myModalLabel" v-if="servicio_elegido_comp">Ingresa el monto</h4>
-           <h4 class="modal-title" id="myModalLabel" v-else>Ingresar movimiento en sucursal @{{sucursal.name}}</h4>
-          <button v-on:click="cancelarIngreso" type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><i class="fas fa-times"></i></span></button>
-          
+      
+      <div class="row">
+        <h4 class="ol-12 sub-titulos-class" v-if="servicio_elegido_comp">Ingresa el monto </h4>
+        <h4 class="modal-title" v-else>Ingresar movimiento de cajaen sucursal @{{sucursal.name}}</h4>
+        <div class="col-12 modal-mensaje-aclarador" v-if="!servicio_elegido_comp">
+          Para ingresar un movimiento debes elegir alguna de las opciones que estan abajo.
         </div>
-        <div class="modal-body text-center"> 
-         <div v-if="servicio_elegido_comp" class="contiene-fase-2-ingreso-de-caja">
-            <div class="formulario-label-fiel">
-                      <label class="formulario-label" for="Nombre"> Detalle a ingresar  </label>
-                      <input type="text" class="formulario-field"  v-model="nombre_a_ingresar" placeholder="Nombre"   />
+      </div>      
+
+      <div class="modal-body">  
+          <div v-if="servicio_elegido_comp" class="contiene-fase-2-ingreso-de-caja">
+            <div class="col-12 mb-2">
+              <label class="formulario-label" for="Nombre"> Detalle a ingresar  </label>
+              <input type="text" class="formulario-field"  v-model="nombre_a_ingresar" placeholder="Nombre"   />
             </div> 
-           
-           <div v-if="$root.aceptaDolares" class="contiene-fase-2-moneda">
-            <div class="flex-row-center flex-justifice-space-around get_width_40">
-              <div class="contiene-opcion-moneda">
+          
+          <div v-if="$root.aceptaDolares" class="contiene-fase-2-moneda">
+            <div class="row mb-2">
+              <div class="col-6">
                 <input type="radio" value="$" v-model="moneda">
                 <label class="moneda-label" for="$">Pesos</label>
               </div>
               
-              <div  class="contiene-opcion-moneda">
+              <div  class="col-6">
                 <input type="radio" value="U$S" v-model="moneda">
                 <label class="moneda-label" for="U$S">Dolares</label>
               </div>
             </div>
-           </div>
+          </div>
 
-           <input type="text" name="" v-model="valor_ingresar" class="ingresar-input-valor"  >
-           <div v-if="valor_ingresar > 0" class="ingreso-caja-aviso">
-             Estás a punto de ingresar ésto: <strong>@{{servicio_elegido.nombre}}</strong>  por un valor de <strong>@{{moneda}} @{{valor_ingresar}} </strong> ¿está bién? . 
-           </div>
-
-           <div v-if="$root.cargando" class="Procesando-text">
+          <div class="row mb-2">
+            <p  class="col-4">@{{moneda}}</p>
+          </div>
+          <div class="col-8 ">
+            <input type="text" name="" v-model="valor_ingresar" class="ingresar-input-valor"  >
+          </div>
+          
+          <p v-if="valor_ingresar > 0" class=" color-text-gris text-center">
+            Estás a punto de ingresar ésto: <strong>@{{servicio_elegido.nombre}}</strong>  por un valor de <strong>@{{moneda}} @{{valor_ingresar}} </strong> ¿está bién? . 
+          </p>
+          <div v-if="$root.cargando" class="Procesando-text">
               <div class="cssload-container">
                     <div class="cssload-tube-tunnel"></div>
               </div>
-           </div>
-           <div v-else class="boton-simple" v-on:click="ingresa_movimiento">
-             @{{$root.boton_aceptar_texto}}
-           </div>
-
-           
+          </div>
+          <div v-else class="boton-simple" v-on:click="ingresa_movimiento">
+            @{{$root.boton_aceptar_texto}}
+          </div>         
         </div>
-        <div v-else class="" class="contiene-ingreso-de-caja-opciones">
-          <div v-if="tipos_de_movimientos.length" 
+        <div v-else class="" class="row ">
+          <div  v-if="tipos_de_movimientos.length" 
                 v-for="movimiento in tipos_de_movimientos" 
                 :key="movimiento.id"
                 :title="movimiento.descripcion_breve"
+                class="col-6"
                 v-on:click="elegir_lo_que_voy_a_agregar(movimiento)" 
-                :class="class_verificar_tipo_saldo(movimiento.tipo_saldo)">
-                @{{movimiento.name}}
+                >                
+                  <div :class="class_verificar_tipo_saldo(movimiento.tipo_saldo)">
+                    @{{movimiento.name}}
+                  </div>    
           </div>
           <div v-if="cargando" class="Procesando-text">
             <div class="cssload-container">
@@ -206,6 +220,59 @@ template:`<span >
             </div>
           </div>           
         </div>
+      </div>     
+
+      <div class="modal-footer">           
+        <button class="modal-default-button"   @click="showModal = !showModal">
+          @{{$root.boton_cancelar_texto}}
+        </button>           
+      </div>
+
+    </div>
+  </div>
+</div>
+</transition>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <div  class="admin-user-boton-Crear mr-lg-2" v-on:click="abrir_modal" title="Ingresar un movimiento de caja">
+       
+   </div>
+
+    <div class="modal fade" id="modal-ingreso-caja" tabindex="+1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+           <h4 class="modal-title" id="myModalLabel" v-if="servicio_elegido_comp">Ingresa el monto</h4>
+           <h4 class="modal-title" id="myModalLabel" v-else>Ingresar movimiento en sucursal @{{sucursal.name}}</h4>
+          <button v-on:click="cancelarIngreso" type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><i class="fas fa-times"></i></span></button>
+          
+        </div>
+        <div class="modal-body text-center"> 
+         
                  
                  
         </div>
