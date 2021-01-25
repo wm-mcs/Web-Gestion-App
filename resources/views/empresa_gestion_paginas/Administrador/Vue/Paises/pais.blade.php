6406e093258ecf09302f48a771ead769df9fe528
Mauricio <ventas@worldmaster.com.uy>
2021-01-25 10:29:26 -0300

    Arreglé tema abrir paises

--- resources/views/empresa_gestion_paginas/Administrador/Vue/Paises/pais.blade.php
+++ resources/views/empresa_gestion_paginas/Administrador/Vue/Paises/pais.blade.php
@@ -11,17 +11,9 @@ var pais = {
   <div class="p-3 mb-3 border-radius-estandar borde-gris background-white h-100">
     <div class="row mx-0">
       <p class="col-10 mb-1 text-color-black ">
-        <b>@{{tipo_de_movimiento.name}}</b>
+        <b>@{{pais.name}}</b>
       </p>
-      <p class="col-2 sub-titulos-class text-center simula-link">
-        <i @click="showModal = true" class="far fa-edit"></i>
-      </p>
-      <p class="col-12 mb-1 ">
-        <small class="py-1 px-2" :class="class_saldo">Saldo <b>@{{tipo_de_movimiento.tipo_saldo}}</b></small>
-      </p>
-      <p class="col-12 mb-0 color-text-gris">
-        <small>@{{tipo_de_movimiento.descripcion_breve}}</small>
-      </p>
+      
     </div>
 
 
