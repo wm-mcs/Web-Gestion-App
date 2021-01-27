83e9c21d95c85b9f7bd44820b4206d360dad163c
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-01-27 15:42:53 -0300

    build css

--- resources/views/empresa_gestion_paginas/Administrador/Vue/Paises/crear-pais.blade.php
+++ resources/views/empresa_gestion_paginas/Administrador/Vue/Paises/crear-pais.blade.php
@@ -1,73 +1,73 @@
 Vue.component("crear-pais", {
-    data: function() {
-        return {
-            cargando: false,
-            datos_a_enviar: {
-                name: "",
-                code: "",
-                currencyCode: "",
-                estado: "si",
-                imagen: ""
-            },
-            showModal: false
-        };
+  data: function() {
+    return {
+      cargando: false,
+      datos_a_enviar: {
+        name: "",
+        code: "",
+        currencyCode: "",
+        estado: "si",
+        imagen: ""
+      },
+      showModal: false
+    };
+  },
+  methods: {
+    limpiar_data_crear: function() {
+      this.datos_a_enviar = {
+        name: "",
+        code: "",
+        currencyCode: "",
+        estado: "si",
+        imagen: ""
+      };
     },
-    methods: {
-        limpiar_data_crear: function() {
-            this.datos_a_enviar = {
-                name: "",
-                code: "",
-                currencyCode: "",
-                estado: "si",
-                imagen: ""
-            };
-        },
-        onImageChange(e) {
-            let files = e.target.files || e.dataTransfer.files;
-            if (!files.length) return;
-            this.createImage(files[0]);
-        },
-        createImage(file) {
-            let reader = new FileReader();
-            let vm = this;
-            reader.onload = e => {
-                vm.datos_a_enviar.imagen = e.target.result;
-            };
-            reader.readAsDataURL(file);
-        },
-        crear: function() {
-            var url = "/crear_pais";
-
-            var data = this.datos_a_enviar;
-
-            var vue = this;
-
-            axios
-                .post(url, data)
-                .then(function(response) {
-                    var data = response.data;
-
-                    if (data.Validacion == true) {
-                        vue.cargando = false;
-                        bus.$emit("se-creo-o-edito-pais", "hola");
-                        vue.showModal = false;
-                        vue.limpiar_data_crear();
-                        $.notify(response.data.Validacion_mensaje, "success");
-                    } else {
-                        vue.cargando = false;
-                        $.notify(response.data.Validacion_mensaje, "error");
-                    }
-                })
-                .catch(function(error) {
-                    vue.cargando = false;
-                    $.notify("Upsssssss.. algo pasó", "error");
-                });
-        }
+    onImageChange(e) {
+      let files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.createImage(files[0]);
     },
-    computed: {},
-    mounted: function() {},
+    createImage(file) {
+      let reader = new FileReader();
+      let vm = this;
+      reader.onload = (e) => {
+        vm.datos_a_enviar.imagen = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    crear: function() {
+      var url = "/crear_pais";
 
-    template: `<span>
+      var data = this.datos_a_enviar;
+
+      var vue = this;
+
+      axios
+        .post(url, data)
+        .then(function(response) {
+          var data = response.data;
+
+          if (data.Validacion == true) {
+            vue.cargando = false;
+            bus.$emit("se-creo-o-edito-pais", "hola");
+            vue.showModal = false;
+            vue.limpiar_data_crear();
+            $.notify(response.data.Validacion_mensaje, "success");
+          } else {
+            vue.cargando = false;
+            $.notify(response.data.Validacion_mensaje, "error");
+          }
+        })
+        .catch(function(error) {
+          vue.cargando = false;
+          $.notify("Upsssssss.. algo pasó", "error");
+        });
+    }
+  },
+  computed: {},
+  mounted: function() {},
+
+  template: `<span>
 
 
   <div class="Boton-Fuente-Chica Boton-Primario-Relleno " @click="showModal = true">
--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Admin/crear-empresa.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Admin/crear-empresa.blade.php
@@ -15,31 +15,28 @@ Vue.component("crear-empresa", {
         rut: "",
         pais: "UY"
       },
+      cargando: false,
       planes: [],
       paises: []
     };
   },
-
   methods: {
     abrir_modal: function() {
       this.getPlanes();
+      this.getPaises();
       $(this.modal)
         .appendTo("body")
         .modal("show");
     },
     crear_empresa_post: function() {
       var validation = confirm("¿Seguros quieres crear la empresa?");
-
       if (!validation) {
         return "";
       }
-
       var url = "/crear_empresa_nueva";
       var vue = this;
       var data = this.data_post;
-
       app.cargando = true;
-
       axios
         .post(url, data)
         .then(function(response) {
@@ -56,54 +53,63 @@ Vue.component("crear-empresa", {
         .catch(function(error) {});
     },
     getPlanes: function() {
-      var url = "/get_planes_empresa";
-
-      var vue = this;
-
+      const url = "/get_planes_empresa";
+      let vue = this;
+      this.cargando = true;
       axios
         .get(url)
         .then(function(response) {
           var data = response.data;
-
           if (data.Validacion == true) {
             vue.planes = data.planes;
+            vue.cargando = false;
             $.notify(response.data.Validacion_mensaje, "success");
+            vue.cargando = false;
           } else {
             $.notify(response.data.Validacion_mensaje, "error");
+            vue.cargando = false;
           }
         })
-        .catch(function(error) {});
+        .catch(function(error) {
+          vue.cargando = false;
+        });
     },
     getPaises: function() {
-      var url = "/get_paises_todos";
-      var vue = this;
+      const url = "/get_paises_todos";
+      let vue = this;
+      this.cargando = true;
 
       axios
         .get(url)
         .then(function(response) {
           var data = response.data;
-
           if (data.Validacion == true) {
             vue.paises = data.Data;
+            vue.cargando = false;
             $.notify(response.data.Validacion_mensaje, "success");
           } else {
+            vue.cargando = false;
             $.notify(response.data.Validacion_mensaje, "error");
           }
         })
-        .catch(function(error) {});
+        .catch(function(error) {
+          vue.cargando = false;
+        });
     },
     get_valor_dependiendo_si_es_conIva: function(valor) {
       if (this.data_post.factura_con_iva == "no") {
         return valor;
       } else {
-        return parseFloat(valor * 1.22).toFixed(1) + " iva inc ";
+        return;
+        parseFloat(valor * 1.22).toFixed(1) + " iva inc ";
       }
     }
   },
   computed: {
     es_con_rut: function() {
       if (this.data_post.factura_con_iva == "si") {
-        return true;
+        return;
+        true;
       } else {
         return false;
       }
@@ -111,189 +117,204 @@ Vue.component("crear-empresa", {
   },
   template: `
 
-<span >
-   <div  style="position:relative;" class="admin-user-boton-Crear" v-on:click="abrir_modal">
-         Crear Empresa       
-   </div>
+<span>
+  <div
+    style="position:relative;"
+    class="admin-user-boton-Crear"
+    v-on:click="abrir_modal"
+  >
+    Crear Empresa
+  </div>
 
-         <div class="modal fade" id="modal-crear-empresa" tabindex="+1" role="dialog" aria-labelledby="myModalLabel">
+  <div
+    class="modal fade"
+    id="modal-crear-empresa"
+    tabindex="+1"
+    role="dialog"
+    aria-labelledby="myModalLabel"
+  >
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-         <div class=""> 
-          <h4 class="modal-title" id="myModalLabel">Crear nueva empresa</h4>
-          <div class="modal-mensaje-aclarador">
-                Aquí tu como vendedor, vas a crear una empresa y el usuario para que la persona pueda comenzar a operar. Por defecto ya quedarás asociado como vendeor, el usuario será asociado como dueño y se creará una Sucursal como principal. Se le enviará un email al usuario con los datos para ingresar.
-
-
-           </div>
+          <div class="">
+            <h4 class="modal-title" id="myModalLabel">Crear nueva empresa</h4>
+            <div class="modal-mensaje-aclarador">
+              Aquí tu como vendedor, vas a crear una empresa y el usuario para
+              que la persona pueda comenzar a operar. Por defecto ya quedarás
+              asociado como vendeor, el usuario será asociado como dueño y se
+              creará una Sucursal como principal. Se le enviará un email al
+              usuario con los datos para ingresar.
+            </div>
           </div>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><i class="fas fa-times"></i></span></button>
-          
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true"><i class="fas fa-times"></i></span>
+          </button>
         </div>
-        <div class="modal-body text-center"> 
-
+        <div class="modal-body text-center">
           {{-- empresa datos --}}
           <div class="contenedor-grupo-datos">
-            <div class="contenedor-grupo-datos-titulo"> Empresa datos</div>
+            <div class="contenedor-grupo-datos-titulo">Empresa datos</div>
             <div class="contenedor-formulario-label-fiel">
-                <div class="formulario-label-fiel">
-                  {!! Form::label('pais', 'País', array('class' => 'formulario-label ')) !!}
+              <p v-if="cargando" class="Procesando-text">Cargando...</p>
+              <div v-esle class="formulario-label-fiel">
+                {!! Form::label('pais', 'País', array('class' =>
+                'formulario-label ')) !!}
 
-                  <select class="formulario-field" v-model="data_post.pais">
-                            
-                            <option v-for="pais in paises" :value="pais.code">
-                                @{{ pais.name }}</option>
-                        </select>
-
-                             </div>                       
-                <div class="formulario-label-fiel">
-                  {!! Form::label('empresa_name', 'Nombre', array('class' => 'formulario-label ')) !!}
-                  {!! Form::text('empresa_name', null ,['class' => 'formulario-field',
-                                                      'v-model' => 'data_post.empresa_name' ]) !!}
-                </div>
-                 <div class="formulario-label-fiel">
-                  {!! Form::label('empresa_email', 'Email', array('class' => 'formulario-label ')) !!}
-                  {!! Form::text('empresa_email', null ,['class' => 'formulario-field',
-                                                       'v-model' => 'data_post.empresa_email' ]) !!}
-                </div>
-                 <div class="formulario-label-fiel">
-                  {!! Form::label('empresa_celular', 'Celular', array('class' => 'formulario-label ')) !!}
-                  {!! Form::text('empresa_celular', null ,['class' => 'formulario-field',
-                                                         'v-model' => 'data_post.empresa_celular' ]) !!}
-                </div>
-
-             
-            </div>
-          </div>    
-
-           {{-- usuario datos --}}
-          <div class="contenedor-grupo-datos">
-            <div class="contenedor-grupo-datos-titulo"> Usuario datos</div>
-            <div class="modal-mensaje-aclarador">
-                Los datos del usuario que va a operar el negocio. Esto le creara el usuario y lo asociara a la empresa que se crea de forma automática.
-            </div>
-            <div class="contenedor-formulario-label-fiel">                       
+                <select class="formulario-field" v-model="data_post.pais">
+                  <option v-for="pais in paises" :value="pais.code">
+                    @{{ pais.name }}</option
+                  >
+                </select>
+              </div>
               <div class="formulario-label-fiel">
-                  {!! Form::label('user_name', 'Nombre del usuario', array('class' => 'formulario-label ')) !!}
-                  {!! Form::text('user_name', null ,['class' => 'formulario-field',
-                                                         'v-model' => 'data_post.user_name' ]) !!}
-                </div>
-                <div class="formulario-label-fiel">
-                  {!! Form::label('user_email', 'Email del usuario', array('class' => 'formulario-label ')) !!}
-                  {!! Form::text('user_email', null ,['class' => 'formulario-field',
-                                                         'v-model' => 'data_post.user_email' ]) !!}
-                </div>
-                <div class="formulario-label-fiel">
-                  {!! Form::label('user_celular', 'Celular del usuario', array('class' => 'formulario-label ')) !!}
-                  {!! Form::text('user_celular', null ,['class' => 'formulario-field',
-                                                         'v-model' => 'data_post.user_celular' ]) !!}
-                </div>
+                {!! Form::label('empresa_name', 'Nombre', array('class' =>
+                'formulario-label ')) !!} {!! Form::text('empresa_name', null
+                ,['class' => 'formulario-field', 'v-model' =>
+                'data_post.empresa_name' ]) !!}
+              </div>
+              <div class="formulario-label-fiel">
+                {!! Form::label('empresa_email', 'Email', array('class' =>
+                'formulario-label ')) !!} {!! Form::text('empresa_email', null
+                ,['class' => 'formulario-field', 'v-model' =>
+                'data_post.empresa_email' ]) !!}
+              </div>
+              <div class="formulario-label-fiel">
+                {!! Form::label('empresa_celular', 'Celular', array('class' =>
+                'formulario-label ')) !!} {!! Form::text('empresa_celular', null
+                ,['class' => 'formulario-field', 'v-model' =>
+                'data_post.empresa_celular' ]) !!}
+              </div>
             </div>
-          </div>   
-           <div class="contenedor-grupo-datos">
-            <div class="contenedor-grupo-datos-titulo"> ¿Factura con Rut?</div>
-            <div class="modal-mensaje-aclarador">
-                Si el cliente solciita factura con ruto marcar en si
-            </div>
+          </div>
 
-               
-                <div class="contiene-fase-2-moneda">
-                 
-                  <div class="crear-empresa-contiene-opciones-planes">
-                    <div class="contiene-opcione-planes">
-                      <input type="radio" value="si" v-model="data_post.factura_con_iva">
-                      <label class="moneda-label" for="si">
-
-                        <span>Si</span> 
-                        
-
-                      </label>
-                    </div>
-                     <div class="contiene-opcione-planes">
-                      <input type="radio" value="no" v-model="data_post.factura_con_iva">
-                      <label class="moneda-label" for="no">
-
-                        <span>No</span> 
-                        
-
-                      </label>
-                    </div>
-                    
-                    
-                  </div>
-                 </div>
-
-             <div v-if="es_con_rut" class="contenedor-formulario-label-fiel">                       
-                <div class="formulario-label-fiel">
-                  {!! Form::label('user_name', 'Razon Social', array('class' => 'formulario-label ')) !!}
-                  {!! Form::text('user_name', null ,['class' => 'formulario-field',
-                                                  'v-model'  => 'data_post.razon_social' ]) !!}
-                </div>
-                <div class="formulario-label-fiel">
-                  {!! Form::label('user_email', 'Rut', array('class' => 'formulario-label ')) !!}
-                  {!! Form::number('user_email', null ,['class'    => 'formulario-field',
-                                                         'v-model' => 'data_post.rut' ]) !!}
-                </div>
-               
-            </div>
-
-          </div>  
+          {{-- usuario datos --}}
           <div class="contenedor-grupo-datos">
-            <div class="contenedor-grupo-datos-titulo"> Plan</div>
+            <div class="contenedor-grupo-datos-titulo">Usuario datos</div>
             <div class="modal-mensaje-aclarador">
-                El plan con el cual va a iniciar. Al comienzo se le otorgara 15 días gratis. Luego si tendra que abonar.
+              Los datos del usuario que va a operar el negocio. Esto le creara
+              el usuario y lo asociara a la empresa que se crea de forma
+              automática.
+            </div>
+            <div class="contenedor-formulario-label-fiel">
+              <div class="formulario-label-fiel">
+                {!! Form::label('user_name', 'Nombre del usuario', array('class'
+                => 'formulario-label ')) !!} {!! Form::text('user_name', null
+                ,['class' => 'formulario-field', 'v-model' =>
+                'data_post.user_name' ]) !!}
+              </div>
+              <div class="formulario-label-fiel">
+                {!! Form::label('user_email', 'Email del usuario', array('class'
+                => 'formulario-label ')) !!} {!! Form::text('user_email', null
+                ,['class' => 'formulario-field', 'v-model' =>
+                'data_post.user_email' ]) !!}
+              </div>
+              <div class="formulario-label-fiel">
+                {!! Form::label('user_celular', 'Celular del usuario',
+                array('class' => 'formulario-label ')) !!} {!!
+                Form::text('user_celular', null ,['class' => 'formulario-field',
+                'v-model' => 'data_post.user_celular' ]) !!}
+              </div>
+            </div>
+          </div>
+          <div class="contenedor-grupo-datos">
+            <div class="contenedor-grupo-datos-titulo">¿Factura con Rut?</div>
+            <div class="modal-mensaje-aclarador">
+              Si el cliente solciita factura con ruto marcar en si
             </div>
 
-                {!! Form::label('user_name', 'Elegir un plan', array('class' => 'formulario-label ')) !!}
-                <div class="contiene-fase-2-moneda">
-                 
-                  <div class="crear-empresa-contiene-opciones-planes">
-                    <div v-for="plan in planes" class="contiene-opcione-planes">
-                      <input type="radio" :value="plan.id" v-model="data_post.plan_id">
-                      <label class="moneda-label" >
+            <div class="contiene-fase-2-moneda">
+              <div class="crear-empresa-contiene-opciones-planes">
+                <div class="contiene-opcione-planes">
+                  <input
+                    type="radio"
+                    value="si"
+                    v-model="data_post.factura_con_iva"
+                  />
+                  <label class="moneda-label" for="si">
+                    <span>Si</span>
+                  </label>
+                </div>
+                <div class="contiene-opcione-planes">
+                  <input
+                    type="radio"
+                    value="no"
+                    v-model="data_post.factura_con_iva"
+                  />
+                  <label class="moneda-label" for="no">
+                    <span>No</span>
+                  </label>
+                </div>
+              </div>
+            </div>
 
-                        <span>@{{plan.name}}</span> 
-                        <span class="contiene-opcione-planes-aclaracion">Por mes @{{plan.moneda}} @{{ get_valor_dependiendo_si_es_conIva(plan.valor)}} hasta @{{plan.cantidad_socios}} socios</span>
+            <div v-if="es_con_rut" class="contenedor-formulario-label-fiel">
+              <div class="formulario-label-fiel">
+                {!! Form::label('user_name', 'Razon Social', array('class' =>
+                'formulario-label ')) !!} {!! Form::text('user_name', null
+                ,['class' => 'formulario-field', 'v-model' =>
+                'data_post.razon_social' ]) !!}
+              </div>
+              <div class="formulario-label-fiel">
+                {!! Form::label('user_email', 'Rut', array('class' =>
+                'formulario-label ')) !!} {!! Form::number('user_email', null
+                ,['class' => 'formulario-field', 'v-model' => 'data_post.rut' ])
+                !!}
+              </div>
+            </div>
+          </div>
+          <p v-if="cargando" class="Procesando-text">Cargando...</p>
+          < v-else div class="contenedor-grupo-datos">
+            <div class="contenedor-grupo-datos-titulo">Plan</div>
+            <div class="modal-mensaje-aclarador">
+              El plan con el cual va a iniciar. Al comienzo se le otorgara 15
+              días gratis. Luego si tendra que abonar.
+            </div>
 
-                      </label>
-                    </div>
-                    
-                    
-                  </div>
-                 </div>
+            {!! Form::label('user_name', 'Elegir un plan', array('class' =>
+            'formulario-label ')) !!}
+            <div class="contiene-fase-2-moneda">
+              <div class="crear-empresa-contiene-opciones-planes">
+                <div v-for="plan in planes" class="contiene-opcione-planes">
+                  <input
+                    type="radio"
+                    :value="plan.id"
+                    v-model="data_post.plan_id"
+                  />
+                  <label class="moneda-label">
+                    <span>@{{ plan.name }}</span>
+                    <span class="contiene-opcione-planes-aclaracion"
+                      >Por mes @{{ plan.moneda }} @{{
+                        get_valor_dependiendo_si_es_conIva(plan.valor)
+                      }}
+                      hasta @{{ plan.cantidad_socios }} socios</span
+                    >
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
 
-          </div> 
-           <br>    
-               
-                  <div v-if="$root.cargando" class="Procesando-text">Procesado...</div>
-                  <div  v-else v-on:click="crear_empresa_post" class="boton-simple">Crear</div>
-                  
-                 
+          <div v-if="$root.cargando" class="Procesando-text">Procesado...</div>
+          <div v-else v-on:click="crear_empresa_post" class="boton-simple">
+            Crear
+          </div>
         </div>
-        <br>
+        <br />
         <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>        
+          <button type="button" class="btn btn-default" data-dismiss="modal">
+            Cerrar
+          </button>
         </div>
       </div>
     </div>
   </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-</span> 
-
- 
+</span>
 
 `
 });
