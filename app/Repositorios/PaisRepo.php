6a61ec7d4f0d00fccbbb2aa52c8824720af70409
Mauricio <ventas@worldmaster.com.uy>
2021-01-26 21:28:12 -0300

    arreglo de paises

--- app/Repositorios/PaisRepo.php
+++ app/Repositorios/PaisRepo.php
@@ -1,34 +1,24 @@
-<?php 
+<?php
 
 namespace App\Repositorios;
 
 use App\Entidades\Pais;
-use Illuminate\Support\Facades\Cache;
 use App\Repositorios\Traits\imagenesTrait;
-
+use Illuminate\Support\Facades\Cache;
 
 class PaisRepo extends BaseRepo
 {
 
-  use imagenesTrait;
-  
-  public function getEntidad()
-  {
-    return new Pais();
-  }
+    use imagenesTrait;
 
-  public function ActualizarCache()
-  {
-    Cache::forget('Paises');
-    Cache::remember('Paises', 10000, function() {
-            return  $this->getEntidadActivasOrdenadasSegun('name','asc');
-    });   
-  }
-    
+    public function getEntidad()
+    {
+        return new Pais();
+    }
 
-   
-  
+    public function ActualizarCache()
+    {
+        Cache::forget('Paises');
+    }
 
-
- 
 }
