f671df05b55ef79d353dc532df9a0e9ae49b60ea
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-01-29 15:34:21 -0300

    plan editar fix

--- app/Http/Controllers/Admin_Empresa/Admin_Empresa_Gestion_Socios_Admin_Vendedores_Controllers.php
+++ app/Http/Controllers/Admin_Empresa/Admin_Empresa_Gestion_Socios_Admin_Vendedores_Controllers.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin_Empresa;
 use App\Http\Controllers\Controller;
 use App\Managers\EmpresaGestion\AgregarMovimientoALaEmpresaManager;
 use App\Managers\EmpresaGestion\CrearEmpresaManager;
-use App\Managers\EmpresaGestion\CrearPlanManager;
 use App\Managers\EmpresaGestion\CrearServicioAEmpresaManager;
 use App\Managers\EmpresaGestion\EditarRenovacionDeEmpresaManager;
 use App\Managers\EmpresaGestion\EliminarMovimientoALaEmpresaManager;
@@ -141,54 +140,6 @@ class Admin_Empresa_Gestion_Socios_Admin_Vendedores_Controllers extends Controll
             'Validacion_mensaje' => 'Se eliminó el estado de cuenta correctamente',
             'empresa' => $Empresa];
 
-    }
-
-    public function get_planes_empresa()
-    {
-        $planes = $this->TipoDeServicioAEmpresaRepo->getServiciosActivosAEmpresas();
-
-        return ['Validacion' => true,
-            'Validacion_mensaje' => 'Planes agregados corectamente',
-            'planes' => $planes];
-    }
-
-    public function crear_plan(Request $Request)
-    {
-        $manager = new CrearPlanManager(null, $Request->all());
-        if (!$manager->isValid()) {
-            return ['Validacion' => false,
-                'Validacion_mensaje' => 'No se pudó crear el plan: ' . $manager->getErrors()];
-        }
-
-        $Propiedades = ['name', 'tipo', 'moneda', 'valor', 'cantidad_socios', 'cantidad_sucursales'];
-
-        $this->TipoDeServicioAEmpresaRepo->setEntidadDato($this->TipoDeServicioAEmpresaRepo->getEntidad(), $Request, $Propiedades);
-
-        $planes = $this->TipoDeServicioAEmpresaRepo->getServiciosActivosAEmpresas();
-
-        return ['Validacion' => true,
-            'Validacion_mensaje' => 'Planes agregados corectamente',
-            'planes' => $planes];
-
-    }
-
-    public function editar_plan_empresa(Request $Request)
-    {
-        $Plan = $Request->get('plan'); //me manda la data en array vue
-        $Entidad = $this->TipoDeServicioAEmpresaRepo->find($Plan['id']);
-
-        //las porpiedades que se van a editar
-        $Propiedades = ['name', 'tipo', 'moneda', 'valor', 'cantidad_socios', 'cantidad_sucursales'];
-
-        foreach ($Propiedades as $Propiedad) {
-            $Entidad->$Propiedad = $Plan[$Propiedad];
-        }
-
-        $Entidad->save();
-
-        return ['Validacion' => true,
-            'Validacion_mensaje' => 'Se editó correctamente ',
-            'planes' => $this->TipoDeServicioAEmpresaRepo->getServiciosActivosAEmpresas()];
     }
 
     //creo empresa
