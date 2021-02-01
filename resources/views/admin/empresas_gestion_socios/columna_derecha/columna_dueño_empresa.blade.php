99a554a2eef765c321f88a7f4bcd9bacd7246006
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-02-01 16:00:22 -0300

    Creando panel analiticias

---
+++ resources/views/admin/empresas_gestion_socios/columna_derecha/columna_dueño_empresa.blade.php
@@ -0,0 +1,14 @@
+
+ <ul class="empresa-contendor-de-secciones">
+
+
+<li class="parrafo-class mb-1">
+ <a href="{{route('get_analiticas')}}" >
+   <small>Analíticas</small>
+ </a>
+</li>
+
+
+
+
+</ul>
