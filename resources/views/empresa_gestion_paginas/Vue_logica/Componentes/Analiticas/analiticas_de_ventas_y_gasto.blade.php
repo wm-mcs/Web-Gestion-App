9219c5c55602cdb788def522651ae52085475ffa
Mauricio <ventas@worldmaster.com.uy>
2021-02-03 08:32:48 -0300

    charts

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Analiticas/analiticas_de_ventas_y_gasto.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/Analiticas/analiticas_de_ventas_y_gasto.blade.php
@@ -104,43 +104,7 @@ Vue.component("analiticas-de-ventas-y-gasto", {
 				});
 		},
 		setData: function() {},
-		calcularSaldo: function() {},
-		graficar: function() {
-			let data = {
-				labels: [
-					"12am-3am",
-					"3am-6pm",
-					"6am-9am",
-					"9am-12am",
-					"12pm-3pm",
-					"3pm-6pm",
-					"6pm-9pm",
-					"9am-12am"
-				],
-				datasets: [
-					{
-						name: "Some Data",
-						type: "bar",
-						values: [25, 40, 30, 35, 8, 52, 17, -4]
-					},
-					{
-						name: "Another Set",
-						type: "line",
-						values: [25, 50, -10, 15, 18, 32, 27, 14]
-					}
-				]
-			};
-
-			const chart = new Chart("#grafica", {
-				// or a DOM element,
-				// new Chart() in case of ES6 module with above usage
-				title: "My Awesome Chart",
-				data: data,
-				type: "axis-mixed", // or 'bar', 'line', 'scatter', 'pie', 'percentage'
-				height: 250,
-				colors: ["#7cd6fd", "#743ee2"]
-			});
-		}
+		calcularSaldo: function() {}
 	},
 	computed: {},
 
