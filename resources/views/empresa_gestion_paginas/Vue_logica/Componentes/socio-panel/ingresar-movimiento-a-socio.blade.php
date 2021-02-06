6f50256eebd4eaa0eebbd7f6bec0bf9183698a8a
Mauricio <ventas@worldmaster.com.uy>
2021-02-06 19:33:08 -0300

    ok

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-panel/ingresar-movimiento-a-socio.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-panel/ingresar-movimiento-a-socio.blade.php
@@ -1,147 +1,131 @@
-Vue.component('ingresar-movimiento-a-socio' ,
-{
-props:[ 'empresa','sucursal','socio'],
-data:function(){
-    return {
-      cargando:false,
-      showModal:false,
-      tipos_de_movimientos:[],    
-      servicio_elegido:'',
-      se_cobra:'si',
-      moneda: '$',
-      valor_ingresar:0,
-      nombre_a_ingresar:'',
-      user:  {!! json_encode(Auth::user()) !!}
-    }
-},
-methods:{
- abrir_modal:function(){
-   
-   this.showModal = true;
-   if(this.tipos_de_movimientos.length === 0)
-   {
-    this.get_tipos_de_movimientos();
-   }   
+Vue.component("ingresar-movimiento-a-socio", {
+	props: ["empresa", "sucursal", "socio"],
+	data: function() {
+		return {
+			cargando: false,
+			showModal: false,
+			tipos_de_movimientos: [],
+			servicio_elegido: "",
+			se_cobra: "si",
+			moneda: "$",
+			valor_ingresar: 0,
+			nombre_a_ingresar: "",
+			user:  {!! json_encode(Auth::user()) !!}
+		};
+	},
+	methods: {
+		abrir_modal: function() {
+			this.showModal = true;
+			if (this.tipos_de_movimientos.length === 0) {
+				this.get_tipos_de_movimientos();
+			}
+		},
+		elegir_lo_que_voy_a_agregar: function(tipo_servicio) {
+			this.servicio_elegido = tipo_servicio;
+			this.nombre_a_ingresar = tipo_servicio.name;
+			this.se_cobra = tipo_servicio.se_paga;
+		},
+		cancelarIngreso: function() {
+			this.showModal = false;
+			this.poner_valor_de_cero();
+		},
+		poner_valor_de_cero: function() {
+			this.servicio_elegido = "";
+			this.moneda = "$";
+			this.valor_ingresar = 0;
+			this.nombre_a_ingresar = "";
+		},
+		class_verificar_tipo_saldo: function(saldo) {
+			if (saldo == "deudor") {
+				return {
+					"contiene-ingreso-opciones-duedor": true,
+					"contiene-ingreso-opciones": true
+				};
+			} else {
+				return {
+					"contiene-ingreso-opciones-acredor": true,
+					"contiene-ingreso-opciones": true
+				};
+			}
+		},
+		ingresa_movimiento: function() {
+			var url = "/ingresar_movimiento_a_socio";
 
- },
- elegir_lo_que_voy_a_agregar:function(tipo_servicio){
-  this.servicio_elegido = tipo_servicio;
-  this.nombre_a_ingresar = tipo_servicio.name;
-  this.se_cobra = tipo_servicio.se_paga;
- },
- cancelarIngreso:function(){
-  this.poner_valor_de_cero();
- },
- poner_valor_de_cero:function(){
-  this.servicio_elegido = '';
-  this.moneda = '$';
-  this.valor_ingresar = 0;
-  this.nombre_a_ingresar = '';
- },
- class_verificar_tipo_saldo:function(saldo){
- if(saldo == 'deudor'){
-  return {
-      
-      'contiene-ingreso-opciones-duedor': true,
-      'contiene-ingreso-opciones':true 
-    }
- }
- else
- {
-  return {
-      
-      'contiene-ingreso-opciones-acredor': true,
-      'contiene-ingreso-opciones':true 
-    }
- }
- },
- ingresa_movimiento:function(){
-      var url = '/ingresar_movimiento_a_socio';
+			var data = {
+				empresa_id: this.empresa.id,
+				moneda: this.moneda,
+				valor: this.valor_ingresar,
+				tipo_saldo: this.servicio_elegido.tipo_saldo,
+				nombre: this.nombre_a_ingresar,
+				socio_id: this.socio.id,
+				paga: this.se_cobra,
+				tipo_de_movimiento_id: this.servicio_elegido.id
+			};
+			var vue = this;
 
-      var data = {  
-                    empresa_id: this.empresa.id,
-                        moneda: this.moneda,  
-                         valor: this.valor_ingresar,
-                    tipo_saldo: this.servicio_elegido.tipo_saldo,
-                        nombre: this.nombre_a_ingresar,
-                      socio_id: this.socio.id,
-                          paga: this.se_cobra,
-         tipo_de_movimiento_id: this.servicio_elegido.id
-                 };  
-      var vue = this;    
+			vue.cargando = true;
 
-     vue.cargando = true;        
+			axios
+				.post(url, data)
+				.then(function(response) {
+					var data = response.data;
 
-     axios.post(url,data).then(function (response){  
-            var data = response.data;  
+					if (data.Validacion == true) {
+						vue.cargando = false;
+						vue.showModal = false;
+						vue.$emit("actualizar_socio", response.data.Socio);
+						bus.$emit("sucursal-set", response.data.sucursal);
+						$.notify(data.Validacion_mensaje, "success");
+						vue.poner_valor_de_cero();
+					} else {
+						vue.cargando = false;
+						$.notify(response.data.Validacion_mensaje, "error");
+					}
+				})
+				.catch(function(error) {
+					vue.cargando = false;
+					$.notify(error, "error");
+				});
+		},
+		get_tipos_de_movimientos: function() {
+			var url = "/getMovimientosParaPanelDeIngresoDeMovimeintoAlSocio";
 
-            if(data.Validacion == true)
-            {
-              vue.cargando = false;
-              vue.showModal = false;
-              vue.$emit('actualizar_socio',response.data.Socio); 
-              bus.$emit('sucursal-set', response.data.sucursal);  
-              $.notify(data.Validacion_mensaje, "success");   
-              vue.poner_valor_de_cero();       
-            }
-            else
-            {
-              vue.cargando = false;
-              $.notify(response.data.Validacion_mensaje, "error");
-            }
-           
-           }).catch(function (error){
-             vue.cargando = false;
-             $.notify(error, "error");                       
-            
-           });
-},
-get_tipos_de_movimientos:function(){
-  var url = '/getMovimientosParaPanelDeIngresoDeMovimeintoAlSocio';
+			var data = {
+				empresa_id: this.empresa.id
+			};
+			var vue = this;
+			vue.cargando = true;
 
-  var data = {  
-               empresa_id: this.empresa.id
-             };  
-  var vue = this; 
-  vue.cargando = true;         
+			axios
+				.post(url, data)
+				.then(function(response) {
+					var data = response.data;
 
-  axios.post(url,data).then(function (response){  
-          var data = response.data;         
+					if (data.Validacion == true) {
+						vue.cargando = false;
+						vue.tipos_de_movimientos = data.Data;
+					} else {
+						vue.cargando = false;
+						$.notify(response.data.Validacion_mensaje, "error");
+					}
+				})
+				.catch(function(error) {
+					vue.cargando = false;
+					$.notify(error, "error");
+				});
+		}
+	},
 
-          if(data.Validacion == true)
-          {
-            vue.cargando = false;
-            vue.tipos_de_movimientos = data.Data;
-          }
-          else
-          { vue.cargando = false;
-            $.notify(response.data.Validacion_mensaje, "error");
-          }
-        
-        }).catch(function (error){
-            vue.cargando = false;
-            $.notify(error, "error");    
-        });
-}
-
-    
-
-},
-
-computed:{
-  servicio_elegido_comp:function(){
-    if(this.servicio_elegido != '')
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-  }
-}
-,
-template:`
+	computed: {
+		servicio_elegido_comp: function() {
+			if (this.servicio_elegido != "") {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	},
+	template: `
 <span >
 <div   class="admin-user-boton-Crear"  v-on:click="abrir_modal" title="Ingreso de movimiento de caja">
     <i class="fas fa-cash-register"></i>   
@@ -150,7 +134,7 @@ template:`
 <div class="modal-mask ">
   <div class="modal-wrapper">
     <div class="modal-container position-relative">
-    <span class="modal-cerrar-icono sub-titulos-class text-center color-text-gris" @click="showModal = !showModal">
+    <span class="modal-cerrar-icono sub-titulos-class text-center color-text-gris" @click="cancelarIngreso">
       <i class="fas fa-times"></i>
     </span>
 
@@ -242,7 +226,7 @@ template:`
       </div>     
 
       <div class="modal-footer">           
-        <button class="modal-default-button"   @click="showModal = !showModal">
+        <button class="modal-default-button"   @click="cancelarIngreso">
           @{{$root.boton_cancelar_texto}}
         </button>           
       </div>
@@ -254,9 +238,4 @@ template:`
 
 
 </span>`
-}
-
-
-
-
-);
+});
