2b319e64bfa07d355ece6fc9377664ec40addedd
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-07-15 15:47:13 -0300

    df

--- resources/views/admin/empresas_gestion_socios/columna_derecha/columna_logo_easy_socios.blade.php
+++ resources/views/admin/empresas_gestion_socios/columna_derecha/columna_logo_easy_socios.blade.php
@@ -15,7 +15,7 @@
                   @if(file_exists($Empresa->path_url_img))
                   <span class="simula_link  disparar-este-form " >
 
-                    <img width="140" height="140" class="logo-easy-columna  rounded-lg mb-0" src="{{$Empresa->url_img}}">
+                    <img width="140" height="140" class="img-fluid rounded-lg mb-0 mt-4" src="{{$Empresa->url_img}}">
                    </span>
 
                    <div class="text-center color-text-gris mt-2 mb-5" style="font-size: 12px;">
